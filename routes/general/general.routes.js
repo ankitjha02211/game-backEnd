63a9b0734a15099c2f2fb734d5a05b159b8a5f84
ankitjha02211 <ankitjha02211@gmail.com>
2021-01-07 10:29:30 +0530

    Refactor 4

--- routes/general/general.routes.js
+++ routes/general/general.routes.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 let general = require("../../controllers/General/general.controller");
 
-router.get("/reset/:id", function (req, res, next) {});
+router.get("/now", function (req, res, next) {});
 
-router.get("/reset/:id", function (req, res, next) {});
+router.get("/leaderboard", function (req, res, next) {});
 
 module.exports = router;
--- routes/user/user.routes.js
+++ routes/user/user.routes.js
@@ -2,10 +2,10 @@ var express = require("express");
 var router = express.Router();
 let user = require("../../controllers/Dashboard/user.controller");
 
-router.get("/reset/:id", function (req, res, next) {});
+router.get("/me", function (req, res, next) {});
 
-router.post("/register", function (req, res, next) {});
+router.post("/game/play", function (req, res, next) {});
 
-router.post("/register", function (req, res, next) {});
+router.post("/game/claim_bonus", function (req, res, next) {});
 
 module.exports = router;
